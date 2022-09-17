1eda57c06b872966dd0b036245944ca7f53f22a9
Scott Bateman <scottb@unb.ca>
2022-09-17 15:07:17 -0300

    Added PushButton Example to demonstrate basic event handling.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module cs3035unb.cs3035examplecode {
     requires javafx.fxml;
 
     exports cs3035unb.cs3035examplecode.HelloWorld;
-    opens cs3035unb.cs3035examplecode.HelloWorld to javafx.fxml;
     exports cs3035unb.cs3035examplecode.HelloWorldFXML;
     opens cs3035unb.cs3035examplecode.HelloWorldFXML to javafx.fxml;
+    exports cs3035unb.cs3035examplecode.PushCounter;
 }
